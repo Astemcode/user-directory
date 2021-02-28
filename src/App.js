bc2f3b2626a44efa432285bbc34ff8e6594cc7d6
Joivre Charles <astemcode@gmail.com>
2021-02-28 18:33:28 -0500

    Updated service worker and app
    The service worker file has been added for offline functionality. The app.js file has also been added into the source folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import DataTable from "./components/Data/dataTable";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <DataTable />
+    </>
+  );
+};
+
+export default App;
